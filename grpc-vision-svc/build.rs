ac3b603787b1e190213d200346b8cb3a2be0a29c
Nikita Petrykin <nvnedu2120@gmail.com>
2024-04-06 02:22:13 +0200

    build: update build.rs to generate file descriptor

--- grpc-vision-svc/build.rs
+++ grpc-vision-svc/build.rs
@@ -1,5 +1,12 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/computer_vision.proto")?;
+    let out_dir: PathBuf = PathBuf::from(env::var("OUT_DIR")?);
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("vision_svc_descriptor.bin"))
+        .compile(&["proto/computer_vision.proto"], &["proto"])?;
 
     Ok(())
 }
